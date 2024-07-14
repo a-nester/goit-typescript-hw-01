b0ed5c2c60fab6024e2624fe8405024800984583
a-nester <a_nester@ukr.net>
2024-07-15 00:01:11 +0300

    fix

--- src/basic/4.ts
+++ src/basic/4.ts
@@ -1,8 +1,8 @@
-function showMessage(message: string) {
+function showMessage(message: string): void {
   console.log(message);
 }
 
-function calc(num1: number, num2: number) {
+function calc(num1: number, num2: number): number {
   return num1 + num2;
 }
 
--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,4 +1,4 @@
-function merge<T, U>(objA: T, objB: U) {
+function merge<T, U>(objA: T, objB: U): T & U {
   return Object.assign({}, objA, objB);
 }
 
